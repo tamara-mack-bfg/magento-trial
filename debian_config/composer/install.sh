d5523ba42fd42998c6ccb048e19cb0e284192a77
tamara-mack-bfg <tamara.mack@bigfishgames.com>
2020-11-03 10:25:45 -0800

    initial test build

---
+++ debian_config/composer/install.sh
@@ -0,0 +1,4 @@
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'c31c1e292ad7be5f49291169c0ac8f683499edddcfd4e42232982d0fd193004208a58ff6f353fde0012d35fdd72bc394') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --version=1.10.17
+php -r "unlink('composer-setup.php');"
